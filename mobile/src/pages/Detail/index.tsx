31909ae13b3eb1a61c289d1c75f6565c1a048839
Bruno Fernandes <magagninpk@gmail.com>
2020-06-06 13:29:40 -0300

    filtro, listagem e detalhes dos pontos de coletas concluidos

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,14 +1,61 @@
-import React from 'react';
-import { Text, View, StyleSheet, TouchableOpacity, Image, SafeAreaView } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, View, StyleSheet, TouchableOpacity, Image, SafeAreaView, Linking } from 'react-native';
 import { Feather as Icon, FontAwesome } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
+import * as MailComposer from 'expo-mail-composer';
+
+import api from '../../services/api';
+
+interface Params {
+  point_id: number;
+}
+
+interface Data {
+  point: {
+    image: string;
+    name: string;
+    email: string;
+    whatsapp: string;
+    city: string;
+    uf: string;
+  };
+  items: {
+    title: string;
+  }[];
+}
 
 const Detail = () => {
+    const [data, setData] = useState<Data>({} as Data);
+
     const navigation = useNavigation();
+    const route = useRoute();
+
+    const routeParams = route.params as Params;
+
+    useEffect(() => {
+      api.get(`points/${routeParams.point_id}`).then(response => {
+        setData(response.data);
+      });
+    }, []);
 
     function handleNavigateBack() {
         navigation.goBack();
+    }
+
+    function handleWhatsapp() {
+      Linking.openURL(`whatsapp://send?phone=${data.point.whatsapp}&text=Tenho interesse sobre coleta de resíduos`);
+    }
+
+    function handleComposeMail() {
+      MailComposer.composeAsync({
+        subject: 'Interesse na coleta de resíduos',
+        recipients: [data.point.email],
+      })
+    }  
+
+    if(!data.point) {
+      return null;
     }
 
     return (
@@ -18,22 +65,24 @@ const Detail = () => {
                 <Icon name="arrow-left" size={20} color="#34cb79"></Icon>
             </TouchableOpacity>    
 
-            <Image style={styles.pointImage} source={{ uri: 'https://images.unsplash.com/photo-1580913428706-c311e67898b3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60'}} />
-            <Text style={styles.pointName}>Mercado do seu Zé</Text>
-            <Text style={styles.pointItems}>Lâmpada e óleo de cozinha</Text>
+            <Image style={styles.pointImage} source={{ uri: data.point.image }} />
+            <Text style={styles.pointName}>{data.point.name}</Text>
+            <Text style={styles.pointItems}>
+             {data.items.map(item => item.title).join(', ')}
+            </Text>
 
             <View style={styles.address}>
                 <Text style={styles.addressTitle}>Endereço</Text>
-                <Text style={styles.addressContent}>RIo Grande do SUl </Text>
+    <Text style={styles.addressContent}>{data.point.city}, {data.point.uf}</Text>
             </View>
         </View>
         <View style={styles.footer}>
-        <RectButton style={styles.button} onPress={() => { }}>
+        <RectButton style={styles.button} onPress={handleWhatsapp}>
             <FontAwesome name="whatsapp" size={20} color="#fff" />
             <Text style={styles.buttonText}>WhatsApp</Text>
         </RectButton>
 
-        <RectButton style={styles.button} onPress={() => { }}>
+        <RectButton style={styles.button} onPress={handleComposeMail}>
             <Icon name="mail" size={20} color="#fff" />
             <Text style={styles.buttonText}>E-mail</Text>
         </RectButton>
@@ -46,7 +95,7 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       padding: 32,
-      paddingTop: 20,
+      paddingTop: 45,
     },
   
     pointImage: {
@@ -93,7 +142,7 @@ const styles = StyleSheet.create({
       borderTopWidth: StyleSheet.hairlineWidth,
       borderColor: '#999',
       paddingVertical: 20,
-      paddingBottom: 0,
+      paddingBottom: 20,
       paddingHorizontal: 32,
       flexDirection: 'row',
       justifyContent: 'space-between'
